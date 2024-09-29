c9d094b7cb327113e13aa94365329b69216f6a59
root <alfredo.445566@gmail.com>
2024-09-29 05:40:36 +0000

    add task 0

---
+++ TypeScript/task_0/js/main.ts
@@ -0,0 +1,31 @@
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+}
+
+const student1: Student = {
+    firstName: 'Alfredo',
+    lastName: 'Spicy',
+    age: 21,
+    location: 'Mexico',
+}
+const student2: Student = {
+    firstName: 'Molly',
+    lastName: 'Dog',
+    age: 6,
+    location: 'Mexico'
+}
+
+const studentsList: Student[] = [student1, student2];
+const table: HTMLTableElement = document.createElement('table');
+document.body.appendChild(table);
+
+studentsList.forEach((student: Student): void => {
+	const newRow: HTMLTableRowElement = table.insertRow();
+	const newRowFirstName: HTMLTableCellElement = newRow.insertCell();
+	const newRowLocation: HTMLTableCellElement = newRow.insertCell();
+	newRowFirstName.innerHTML = student.firstName;
+	newRowLocation.innerHTML = student.location;
+})
